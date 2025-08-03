af7f4ddfbdd6d1901083edb8144cf4b5af1e3312
Josh <josh@Caseys-MacBook-Pro.local>
2025-08-02 21:59:42 -0400

    More river scene setup

---
+++ Assets/Scripts/Controllers/ui/TimeDisplayController.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Controllers.ui {
+    public class TimeDisplayController : MonoBehaviour {
+        public TMP_Text timeText;
+
+        private void ShowTimeText() {
+            // Shows the time elapsed since game start in minutes:seconds format
+            float timeElapsed = Time.timeSinceLevelLoad;
+            int minutes = Mathf.FloorToInt(timeElapsed / 60);
+            int seconds = Mathf.FloorToInt(timeElapsed % 60);
+            timeText.text = $"{minutes:D2}:{seconds:D2}";
+        }
+
+        private void Update()
+        {
+            ShowTimeText();
+        }
+    }
+}
